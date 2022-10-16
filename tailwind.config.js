ab086209f5110bbcb02b2c94ced960c38272a21c
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-10-16 16:53:36 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      primary: 'Poppins',
+    },
+    extend: {
+      backgroundImage: {
+        gradientBg: "url('/bg.png')",
+      },
+      keyframes: {
+        shake: {
+          '0%': {
+            transform: 'translate(3px, 0)',
+          },
+          '50%': {
+            transform: 'translate(-3px, 0)',
+          },
+          '100%': {
+            transform: 'translate(0, 0)',
+          },
+        },
+      },
+      animation: {
+        shake: 'shake 150ms 2 linear',
+      },
+    },
+  },
+  plugins: [],
+};
